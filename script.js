8a38459853fc564ec68d68339d5308cdae9722c2
LilDicky322 <koroccori@gmail.com>
2024-12-02 22:52:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,44 +6,44 @@ const imageContainer = document.getElementById('image-container');
 
 // Array de rutas de imágenes (asegúrate de tener todas las imágenes en la carpeta 'img')
 const imageURLs = [
-  '/322/img/imagen1.png',
-  '/322/img/imagen2.png',
-  '/322/img/imagen3.png',
-  '/322/img/imagen4.png',
-  '/322/img/imagen5.png',
-  '/322/img/imagen6.png',
-  '/322/img/imagen7.png',
-  '/322/img/imagen8.png',
-  '/322/img/imagen9.png',
-  '/322/img/imagen10.png',
-  '/322/img/imagen11.png',
-  '/322/img/imagen12.png',
-  '/322/img/imagen13.png',
-  '/322/img/imagen14.png',
-  '/322/img/imagen15.png',
-  '/322/img/imagen16.png',
-  '/322/img/imagen17.png',
-  '/322/img/imagen18.png',
-  '/322/img/imagen19.png',
-  '/322/img/imagen20.png',
-  '/322/img/imagen21.png',
-  '/322/img/imagen22.png',
-  '/322/img/imagen23.png',
-  '/322/img/imagen24.png',
-  '/322/img/imagen25.png',
-  '/322/img/imagen26.png',
-  '/322/img/imagen27.png',
-  '/322/img/imagen28.png',
-  '/322/img/imagen29.png',
-  '/322/img/imagen30.png'
+  'img/imagen1.png',
+  'img/imagen2.png',
+  'img/imagen3.png',
+  'img/imagen4.png',
+  'img/imagen5.png',
+  'img/imagen6.png',
+  'img/imagen7.png',
+  'img/imagen8.png',
+  'img/imagen9.png',
+  'img/imagen10.png',
+  'img/imagen11.png',
+  'img/imagen12.png',
+  'img/imagen13.png',
+  'img/imagen14.png',
+  'img/imagen15.png',
+  'img/imagen16.png',
+  'img/imagen17.png',
+  'img/imagen18.png',
+  'img/imagen19.png',
+  'img/imagen20.png',
+  'img/imagen21.png',
+  'img/imagen22.png',
+  'img/imagen23.png',
+  'img/imagen24.png',
+  'img/imagen25.png',
+  'img/imagen26.png',
+  'img/imagen27.png',
+  'img/imagen28.png',
+  'img/imagen29.png',
+  'img/imagen30.png'
 ];
 
 // Array de enlaces fijos relacionados con cada imagen
 const imageLinks = [
   //1
-  '322/pdf/Querida.pdf',
+  'pdf/Querida.pdf',
   //2
-  '322/pdf/Madera.pdf',
+  'pdf/Madera.pdf',
   //3
   'https://www.youtube.com/watch?v=bp_o07ND3Qk&t=62s',
   //4
@@ -53,17 +53,17 @@ const imageLinks = [
   //6
   'https://www.youtube.com/watch?v=zLy2KBjyq3E',
   //7
-  '/322/pdf/PARTE_DE_TI.pdf',
+  'pdf/PARTE_DE_TI.pdf',
   //8
   'https://www.youtube.com/watch?v=NdDUQ4v-3h4',
   //9
   'https://www.youtube.com/watch?v=8H1iy5IR-2I',
   //10
-  '/322/pdf/Whisky.pdf',
+  'pdf/Whisky.pdf',
   //11
   'https://www.youtube.com/watch?v=BNZvVkS1ZY4',
     //12
-  '/322/pdf/Amaras.pdf',
+  'pdf/Amaras.pdf',
   //13
   'https://www.youtube.com/watch?v=tGu1LM7gX4Y',
   //14
@@ -73,7 +73,7 @@ const imageLinks = [
     //16
   'https://scontent.ftcq3-1.fna.fbcdn.net/v/t1.6435-9/173605622_778189956147139_1648726165880807099_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGvqW8BO7jtqwWuxgHJszO3CS6T-JCko5QJLpP4kKSjlKkggKcTmgwnt6qq220Xz-gDc7cWOStigS6N4YhIgznI&_nc_ohc=cTnSQYHfWZUQ7kNvgEH8T3k&_nc_zt=23&_nc_ht=scontent.ftcq3-1.fna&_nc_gid=AfLI6CA16nDjuCJbh2jerUH&oh=00_AYBjQovM7m3Bu2vs-2H60z0H9uReo2lg0d-stYl1LL6QlQ&oe=676C7ED5',
     //17
-  '/322/pdf/MISMO.pdf',
+  'pdf/MISMO.pdf',
     //18
   'https://www.youtube.com/watch?v=2zOGc6UcIUs',
   //19
